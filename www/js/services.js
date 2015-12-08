6a2e2c02b85459d319187e18d2351798c49f8336
= <=>
2015-12-08 11:11:02 +0530

    Image path changed to own website

--- www/js/services.js
+++ www/js/services.js
@@ -9,31 +9,31 @@ angular.module('starter.services', [])
     name: 'Vinod Kumar',
     posted_on: 'November 05, 1955',
 	description: 'A facebook friend just joined Swish',
-    face: 'http://ionicframework.com/img/docs/mcfly.jpg'
+    face: 'http://parssv.com/swish/images/3j5HUXMY.png'
   }, {
     id: 1,
     name: 'Soru Khurana',
     posted_on: 'November 05, 1955',
 	description: 'Accepted your friend request',
-    face: 'http://ionicframework.com/img/docs/mcfly.jpg'
+    face: 'http://parssv.com/swish/images/3j5HUXMY.png'
   }, {
     id: 2,
     name: 'Sonam',
     posted_on: 'November 05, 1955',
 	description: 'Has checked in YMCA',
-    face: 'http://ionicframework.com/img/docs/mcfly.jpg'
+    face: 'http://parssv.com/swish/images/3j5HUXMY.png'
   }, {
     id: 3,
     name: 'Sajan',
     posted_on: 'November 05, 1955',
 	description: 'Has checked in New York sport club',
-    face: 'http://ionicframework.com/img/docs/mcfly.jpg'
+    face: 'http://parssv.com/swish/images/3j5HUXMY.png'
   }, {
     id: 4,
     name: 'Ben Sparrow',
     posted_on: 'November 05, 1955',
 	description: 'Has checked in New York sport club',
-    face: 'http://ionicframework.com/img/docs/mcfly.jpg'
+    face: 'http://parssv.com/swish/images/3j5HUXMY.png'
   }];
 
   return {
@@ -64,27 +64,27 @@ angular.module('starter.services', [])
     id: 0,
     name: 'Ben Sparrow',
     lastText: 'You on your way?',
-    face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
+    face: 'http://parssv.com/swish/images/3j5HUXMY.png'
   }, {
     id: 1,
     name: 'Max Lynx',
     lastText: 'Hey, it\'s me',
-    face: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460'
+    face: 'http://parssv.com/swish/images/3j5HUXMY.png'
   }, {
     id: 2,
     name: 'Adam Bradleyson',
     lastText: 'I should buy a boat',
-    face: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
+    face: 'http://parssv.com/swish/images/3j5HUXMY.png'
   }, {
     id: 3,
     name: 'Perry Governor',
     lastText: 'Look at my mukluks!',
-    face: 'https://pbs.twimg.com/profile_images/598205061232103424/3j5HUXMY.png'
+    face: 'http://parssv.com/swish/images/3j5HUXMY.png'
   }, {
     id: 4,
     name: 'Mike Harrington',
     lastText: 'This is wicked good ice cream.',
-    face: 'https://pbs.twimg.com/profile_images/578237281384841216/R3ae1n61.png'
+    face: 'http://parssv.com/swish/images/3j5HUXMY.png'
   }];
 
   return {
@@ -113,27 +113,27 @@ angular.module('starter.services', [])
     id: 0,
     name: 'Ben Sparrow',
     lastText: 'You on your way?',
-    face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
+    face: 'http://parssv.com/swish/images/3j5HUXMY.png'
   }, {
     id: 1,
     name: 'Max Lynx',
     lastText: 'Hey, it\'s me',
-    face: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460'
+    face: 'http://parssv.com/swish/images/3j5HUXMY.png'
   }, {
     id: 2,
     name: 'Adam Bradleyson',
     lastText: 'I should buy a boat',
-    face: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
+    face: 'http://parssv.com/swish/images/3j5HUXMY.png'
   }, {
     id: 3,
     name: 'Perry Governor',
     lastText: 'Look at my mukluks!',
-    face: 'https://pbs.twimg.com/profile_images/598205061232103424/3j5HUXMY.png'
+    face: 'http://parssv.com/swish/images/3j5HUXMY.png'
   }, {
     id: 4,
     name: 'Mike Harrington',
     lastText: 'This is wicked good ice cream.',
-    face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
+    face: 'http://parssv.com/swish/images/3j5HUXMY.png'
   }];
 
   return {
